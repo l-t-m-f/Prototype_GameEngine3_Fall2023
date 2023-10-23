a81eabaefb399ddd35dc7f778c886f61323f8118
LTMF <ltmf1990@protonmail.com>
2023-10-23 08:58:10 -0400

    integration of subsystem p1.

---
+++ Source/Prototype/GameProgress/GameProgressSubsystem.cpp
@@ -0,0 +1,15 @@
+﻿#include "GameProgressSubsystem.h"
+
+void
+UGameProgressSubsystem::Initialize(FSubsystemCollectionBase &Collection)
+{
+   Super::Initialize(Collection);
+   UE_LOG(LogTemp, Warning, TEXT("Progress subsystem - Initialized"));
+}
+
+void
+UGameProgressSubsystem::Deinitialize()
+{
+   Super::Deinitialize();
+   UE_LOG(LogTemp, Warning, TEXT("Progress subsystem - Deinitialized"));
+}
---
+++ Source/Prototype/GameProgress/GameProgressSubsystem.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameProgressSubsystem.generated.h"
+
+UCLASS()
+class PROTOTYPE_API UGameProgressSubsystem : public UGameInstanceSubsystem
+{
+   GENERATED_BODY()
+   
+   virtual void Initialize(FSubsystemCollectionBase &Collection) override;
+
+   virtual void Deinitialize() override;
+
+   UFUNCTION(BlueprintCallable)
+   FString GetMessage() const {
+      return Message;
+   }
+   
+   FString Message = "I am the Progress!!!!";
+   
+};
