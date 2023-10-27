fdcf4a1ac9e191d323b8b6d1a5ae627eac0e1379
LTMF <ltmf1990@protonmail.com>
2023-10-27 11:53:38 -0400

    dialog choices system improvements.

--- Source/Prototype/GameProgress/GameProgressSubsystem.cpp
+++ Source/Prototype/GameProgress/GameProgressSubsystem.cpp
@@ -1,5 +1,7 @@
 ﻿#include "GameProgressSubsystem.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
 void
 UGameProgressSubsystem::Initialize(FSubsystemCollectionBase &Collection)
 {
@@ -7,9 +9,8 @@ UGameProgressSubsystem::Initialize(FSubsystemCollectionBase &Collection)
    UE_LOG(LogTemp, Warning, TEXT("Progress subsystem - Initialized"));
 
    NPC_Details = TMap<FName, FNPC_Details>();
-   NPC_Details.Add(FName("Test1"), FNPC_Details());
-   NPC_Details.Add(FName("Test2"), FNPC_Details(2,3));
-   
+   NPC_Details.Add(FName("Test1"), FNPC_Details(0, 0));
+   NPC_Details.Add(FName("Test2"), FNPC_Details(2, 3));
 }
 
 void
@@ -35,4 +36,31 @@ FNPC_Details
 UGameProgressSubsystem::GetNPC_Details(const FName &InName) const
 {
    return NPC_Details.FindRef(InName);
+}
+
+void
+UGameProgressSubsystem::UpdateNPC_Details(const FName &InName,
+                                          EDialogAction InAction)
+{
+   FNPC_Details Details = NPC_Details.FindRef(InName);
+
+   switch(InAction)
+      {
+      case EDialogAction::ACTION_CloseDialog:
+         break;
+      case EDialogAction::ACTION_NextQuestLine:
+         Details.CurrentQuestLine = Details.CurrentQuestLine + 1;
+         break;
+      case EDialogAction::ACTION_NextSlide:
+         Details.CurrentDialogSlide = Details.CurrentDialogSlide + 1;
+         break;
+      default:;
+      }
+
+   UKismetSystemLibrary::PrintString(
+      this, InName.ToString() + " "
+               + FString::FromInt(Details.CurrentQuestLine) + " "
+               + FString::FromInt(Details.CurrentDialogSlide));
+
+   NPC_Details.Add(InName, Details);
 }
