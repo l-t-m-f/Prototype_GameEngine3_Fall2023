d90931801a55c0207b19c0c8aaf3fb82d471a90c
LTMF <ltmf1990@protonmail.com>
2023-11-24 11:38:26 -0500

    GenericGraph demo

---
+++ Plugins/GenericGraph/Source/GenericGraphEditor/GenericGraphEditor.Build.cs
@@ -0,0 +1,64 @@
+using UnrealBuildTool;
+
+public class GenericGraphEditor : ModuleRules
+{
+	public GenericGraphEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		bLegacyPublicIncludePaths = false;
+		ShadowVariableWarningLevel = WarningLevel.Error;
+
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+                // ... add other private include paths required here ...
+                "GenericGraphEditor/Private",
+				"GenericGraphEditor/Public",
+			}
+			);
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+                "Engine",
+                "UnrealEd",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+                "GenericGraphRuntime",
+                "AssetTools",
+                "Slate",
+                "InputCore",
+                "SlateCore",
+                "InputCore",
+                "GraphEditor",
+                "PropertyEditor",
+                "EditorStyle",
+                "Kismet",
+                "KismetWidgets",
+                "ApplicationCore",
+				"ToolMenus",
+				"Projects",
+				// ... add private dependencies that you statically link with here ...
+			}
+			);
+
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+            }
+			);
+	}
+}
---
+++ Plugins/GenericGraph/Source/GenericGraphEditor/Public/GenericGraphAssetEditor/EditorCommands_GenericGraph.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorStyleSet.h"
+#include "Framework/Commands/Commands.h"
+
+class GENERICGRAPHEDITOR_API FEditorCommands_GenericGraph : public TCommands<FEditorCommands_GenericGraph>
+{
+public:
+	/** Constructor */
+	FEditorCommands_GenericGraph()
+		: TCommands<FEditorCommands_GenericGraph>("GenericGraphEditor", NSLOCTEXT("Contexts", "GenericGraphEditor", "Generic Graph Editor"), NAME_None, FAppStyle::GetAppStyleSetName())
+	{
+	}
+	
+	TSharedPtr<FUICommandInfo> GraphSettings;
+	TSharedPtr<FUICommandInfo> AutoArrange;
+
+	virtual void RegisterCommands() override;
+};
